3b1e92ffd6b5c1d9dd63125c4b992086fe21ee16
phamvinhnga <ngapham07051996@gmail.com>
2018-08-14 14:30:50 +0700

    update config languae all

--- Corals/core/Foundation/config/language.php
+++ Corals/core/Foundation/config/language.php
@@ -115,6 +115,6 @@ return [
         ['short' => 'tr', 'long' => 'tr-TR', 'english' => 'Turkish', 'native' => 'Türkçe'],
         ['short' => 'tw', 'long' => 'zh-TW', 'english' => 'Chinese (T)', 'native' => '繁體中文'],
         ['short' => 'uk', 'long' => 'uk-UA', 'english' => 'Ukrainian', 'native' => 'Українська'],
-        ['short' => 'vi', 'long' => 'vi-VN', 'english' => 'Vietnamese', 'native' => 'Tiếng Việt'],
+        ['short' => 'vi', 'long' => 'vi-VI', 'english' => 'Vietnamese', 'native' => 'Tiếng Việt'],
     ],
 ];
